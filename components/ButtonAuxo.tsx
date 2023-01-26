36fdd5253064800768cd79eb0a0494ae8985bcb4
Camilo Valencia <acvalencia.contact@gmail.com>
2023-01-25 19:04:32 -0500

    hot fix

--- components/ButtonAuxo.tsx
+++ components/ButtonAuxo.tsx
@@ -1,7 +1,6 @@
 export const ButtonAuxo = ({variant, onClick, children}) => {
 
   return (
-    console.log(variant),
       <button
         className={`px-4 py-2 ${variant==='primary'?'bg-greenBgAuxo':'bg-whiteAuxo'} rounded-lg shadow-sm `}
         onClick={ onClick }
